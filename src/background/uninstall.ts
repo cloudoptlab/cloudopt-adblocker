d1e51ef5b78d67c5e9f458a7297d7036f0d8f5a1
Andy <myts.designer@gmail.com>
2019-12-09 16:16:51 +0800

    feat: \increase listening to installation events.

--- src/background/uninstall.ts
+++ src/background/uninstall.ts
@@ -1,1 +1,5 @@
+import * as tabs from '../core/tab'
+chrome.runtime.onInstalled.addListener((details) => {
+    tabs.open('/guide.html')
+})
 chrome.runtime.setUninstallURL('https://wj.qq.com/s2/3072248/dc7c/')
