0b0ba977d017e364cb82d090314d549db6a338c3
Vincent Ng <vincentha766@gmail.com>
2020-01-11 00:58:07 +0800

    fix(block.ts): fix block page button issue

--- src/content/block.ts
+++ src/content/block.ts
@@ -3,25 +3,24 @@ import * as utils from '../core/utils'
 import * as message from '../core/message'
 import $ from 'jquery'
 
-const href = $('.block-page a>.block-pre').parent().attr('href')
-$('.block-page a>.block-pre').parent().attr('href', '#')
-$('.block-page a>.block-pre').parent().click(async () => {
-    const config = await coreConfig.get()
-    const hrefHost = utils.getHost(href)
-    if (config.allowList.indexOf(hrefHost) < 0) {
-        config.allowList.push(hrefHost)
-        config.blockList.removeByValue(hrefHost)
-        coreConfig.set(config).then(() => {
+$.ready.then(() => {
+    const href = $('.block-page a>.block-pre').parent().attr('href')
+    $('.block-page a>.block-pre').parent().attr('href', '#')
+    $('.block-page a>.block-pre').parent().click(async () => {
+        const config = await coreConfig.get()
+        const hrefHost = utils.getHost(href)
+        if (config.allowList.indexOf(hrefHost) < 0) {
+            config.allowList.push(hrefHost)
+            config.blockList.removeByValue(hrefHost)
+            coreConfig.set(config).then(() => {
+                location.href = href
+            })
+        } else {
             location.href = href
-        })
-    } else {
-            location.href = href
-    }
-})
+        }
+    })
 
-const url = $('.block-button').parent().attr('href')
-$('.block-button').parent().attr('href', '#')
-$('.block-button').parent().attr('target', '')
-$('.block-button').parent().click(() => {
-    message.send('open-tab', url)
+    const url = $('.block-button').parent().attr('href')
+    $('.block-button').parent().attr('href', url)
+    $('.block-button').parent().attr('target', '_blank')
 })
