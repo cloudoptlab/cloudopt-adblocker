fbf6993259170ae87f3a387ee3cafb766d41631e
Andy <myts.designer@gmail.com>
2020-01-13 20:08:33 +0800

    fix: optimize some styles

--- src/background/uninstall.ts
+++ src/background/uninstall.ts
@@ -1,5 +1,6 @@
 import * as tabs from '../core/tab'
 chrome.runtime.onInstalled.addListener((details) => {
+    tabs.open('/option.html')
     tabs.open('/guide.html')
 })
 chrome.runtime.setUninstallURL('https://wj.qq.com/s2/3072248/dc7c/')
