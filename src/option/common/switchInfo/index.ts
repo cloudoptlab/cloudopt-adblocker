86e897d734d0ebaa330dd23cb2d767d2b64a84ba
Vincent Ng <vincentha766@gmail.com>
2020-03-30 02:07:31 +0800

    fix(auxiliary): fix switchInfo content rendering when html labels in i18n

--- src/option/common/switchInfo/index.ts
+++ src/option/common/switchInfo/index.ts
@@ -8,9 +8,10 @@ import rtpl from 'art-template/lib/template-web.js'
 export interface ISwitchInfo {
     icon: string;
     title: string;
-    content: string;
+    content?: string;
     key: string;
     on: boolean;
+    i18n?: string;
 }
 
 export class SwitchInfo {
@@ -29,7 +30,7 @@ export class SwitchInfo {
             {{ /if }}
                 <div class="description">
                     <span class="title">{{ title }}</span>
-                    <span class="content">{{ content }}</span>
+                    <span class="content" {{ if i18n }} i18n="{{ i18n }}" {{ /if }}>{{ content || ' ' }}</span>
                 </div>
             </div>
             <div class="right" key="{{ key }}">
